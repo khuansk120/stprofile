1d4f4e7b10518159603288dd9c1838489af748c3
Wilawan <wilawan072@gmail.com>
2018-05-03 17:22:55 +0700

    editstudent

--- pages/linkstu.vue
+++ pages/linkstu.vue
@@ -1,48 +1,27 @@
 <template>
-  <div>
-    <h1>  Data  Students </h1><br> 
-    <h1>**************** </h1><br>
-    <input type="text" :value="name">
-    <button @click="addStudent">Add</button>
-    <input type="text" :value="dep">
-    <button @click="addDepartment">Add</button>
-    <ul>
-       <li v-for="st in student" :key="st.code" >
-        {{ st.code }} {{ st.name }} {{ st.dep }}
-      </li>
-    </ul>
-  </div>
+<div>
+  <h1>ข้อมูลนักศึกษา</h1><br>
+  <h1>***************</h1><br>
+  <ul>
+    <li v-for="n in 10" :key="n" ><nuxt-link :to="'/No0'+n">No0{{n}}</nuxt-link> 
+    <v-btn color="yellow"  @click="showStu"><v-btn flat icon color="pink"><v-icon>favorite</v-icon></v-btn>ดูประวัตินักศึกษา</v-btn>
+    <v-btn color="red" @click="editStu" ><v-btn flat icon color="blue"><v-icon>thumb_up</v-icon></v-btn>แก้ไขประวัตินักศึกษา</v-btn></li>
+    <br>
+   
+  </ul>
+ 
+ </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      name: '',
-      code: '',
-      dep: '',
-      student: [
-        { code: '001', name: 'Name1', dep: 'Dep1' },
-        { code: '002', name: 'Name2', dep: 'Dep2' },
-        { code: '003', name: 'Name3', dep: 'Dep3' },
-        { code: '004', name: 'Name4', dep: 'Dep4' },
-        { code: '005', name: 'Name5', dep: 'Dep5' },
-        { code: '006', name: 'Name6', dep: 'Dep6' },
-        { code: '007', name: 'Name7', dep: 'Dep7' },
-        { code: '008', name: 'Name8', dep: 'Dep8'},
-        { code: '009', name: 'Name9', dep: 'Dep9' },
-        { code: '010', name: 'Name10', dep: 'Dep10' },
-       
-      ],
-    } 
-  },//data
   methods: {
-    addStudent() {
-      this.student.push({
-        code: this.student.length+1,
-        name: this.name,
-        dep: this.dep,
-      })
-    }
+      showStu(){
+        this.$router.push('/stview')
+      },
+       editStu(){
+        this.$router.push('/pageEdit')
+       },
   }
 }
 </script>
+
