0859a9d1a8495fb9d13eda8989de79c00a0d6122
Khuan <khuan120@gmail.com>
2018-05-03 17:06:11 +0700

    viewandlayout

--- pages/stview.vue
+++ pages/stview.vue
@@ -1,10 +1,49 @@
 <template>
 <div>
   <h1 class="header">ประวัตินักศึกษา</h1>
-  <h1 class="text1">ชื่อ-นามสกุล</h1>
-  <h1 class="text1">แผนกวิชา</h1>
+  <h1 class="text1">ชื่อ</h1><input type="text" :value="fname">
+  <h1 class="text1">นามสกุล</h1><input type="text" :value="lname">
+  <h1 class="text1">แผนกวิชา</h1><input type="text" :value="dname">
+      <nuxt-link to="/pageEdit.vue"><v-btn
+      color="info"
+      :loading="loading4"
+      @click.native="loader = 'loading4'"
+      :disabled="loading4"
+    >
+      แก้ไขข้อมูลนักศึกษา
+      <span slot="loader" class="custom-loader">
+        <v-icon light>cached</v-icon>
+      </span>
+    </v-btn></nuxt-link>
+          <nuxt-link to="/linkdata"><v-btn
+      color="info"
+      :loading="loading4"
+      @click.native="loader = 'loading4'"
+      :disabled="loading4"
+    >
+      กลับหน้าหลัก
+      <span slot="loader" class="custom-loader">
+        <v-icon light>cached</v-icon>
+      </span>
+    </v-btn></nuxt-link>
+  <!-- <nuxt></nuxt>  -->
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      fname:'กรุณาใส่ชื่อ',
+      lname:'กรุณาใส่นามสกุล',
+      dname:'กรุณาใส่แผนกวิชา',
+          }
+    },
+    layout: 'stvie',
+
+        
+}
+</script>
+
 <style scoped>
 .header{
   font-size: 28px;color: blue;
