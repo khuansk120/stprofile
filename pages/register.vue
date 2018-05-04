0778281c99a1d79b255dd2712bc6ff48e46be7a5
Khuan <khuan120@gmail.com>
2018-05-04 09:02:15 +0700

    upregis

--- pages/register.vue
+++ pages/register.vue
@@ -1,13 +1,30 @@
 <template>
-   <v-flex xs12>
-                <v-text-field
-                  label="lname"
-                  v-model="lname"
-                  :rules="[rules.required]"
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
-      </v-flex>
+<div>
+    <v-container fluid>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-text-field label="lname" v-model="lname" :rules="[rules.required]"></v-text-field>
+            </v-flex>
+    
+        </v-layout>
+    </v-container>
+      </div>
 </template>
+<script>
+  export default {
+      layout:'stvie',
+    data () {
+      return {
+        title: 'Preliminary report',
+        email: '',
+        rules: {
+          required: (value) => !!value || 'Required.',
+          email: (value) => {
+            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            return pattern.test(value) || 'Invalid e-mail.'
+          }
+        }
+      }
+    }
+  }
+</script>
