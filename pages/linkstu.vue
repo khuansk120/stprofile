76fc1e6957cccf375e2a6ecf86afde9426907209
Wilawan <wilawan072@gmail.com>
2018-05-04 08:46:48 +0700

    update

--- pages/linkstu.vue
+++ pages/linkstu.vue
@@ -1,19 +1,19 @@
 <template>
 <div>
-  <h1>ข้อมูลนักศึกษา</h1><br>
-  <h1>***************</h1><br>
+  
   <ul>
     <li v-for="n in 10" :key="n" ><nuxt-link :to="'/No0'+n">No0{{n}}</nuxt-link> 
     <v-btn color="yellow"  @click="showStu"><v-btn flat icon color="pink"><v-icon>favorite</v-icon></v-btn>ดูประวัตินักศึกษา</v-btn>
     <v-btn color="red" @click="editStu" ><v-btn flat icon color="blue"><v-icon>thumb_up</v-icon></v-btn>แก้ไขประวัตินักศึกษา</v-btn></li>
     <br>
    
-  </ul>
+   </ul>
  
  </div>
 </template>
 <script>
 export default {
+  layout: 'layerlinkstu',
   methods: {
       showStu(){
         this.$router.push('/stview')
