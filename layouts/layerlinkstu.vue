cdb0c27aabbbd70b27521be2403e06fcb9a6e0e2
Wilawan <wilawan072@gmail.com>
2018-05-04 08:42:39 +0700

    layout link stu

--- layouts/layerlinkstu.vue
+++ layouts/layerlinkstu.vue
@@ -26,15 +26,15 @@
     </v-navigation-drawer>
     <v-toolbar color="indigo" dark fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>ข้อมูลนักศึกษา</v-toolbar-title>
     </v-toolbar>
     <v-content>
       <v-container fluid fill-height>
         <v-layout
           justify-center
           align-center
-        >
-          <v-flex text-xs-center>
+        ><nuxt></nuxt>
+          <!-- <v-flex text-xs-center>
             <v-tooltip left>
               <v-btn icon large :href="source" target="_blank" slot="activator">
                 <v-icon large>code</v-icon>
@@ -47,7 +47,7 @@
               </v-btn>
               <span>Codepen</span>
             </v-tooltip>
-          </v-flex>
+          </v-flex> -->
         </v-layout>
       </v-container>
     </v-content>
