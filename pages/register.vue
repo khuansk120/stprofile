2f04287c93d63f2ee2018d5a1b610784cc203566
Khuan <khuan120@gmail.com>
2018-05-04 08:37:52 +0700

    addregisandupdate

---
+++ pages/register.vue
@@ -0,0 +1,13 @@
+<template>
+   <v-flex xs12>
+                <v-text-field
+                  label="lname"
+                  v-model="lname"
+                  :rules="[rules.required]"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+</template>
