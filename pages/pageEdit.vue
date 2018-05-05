5806b9f8d0ed833cbe8495a7a2c8c0bf87c7cef3
worawek <worawekch@gmail.com>
2018-05-05 15:23:47 +0700

    update

--- pages/pageEdit.vue
+++ pages/pageEdit.vue
@@ -1,30 +1,10 @@
 <template>
   <div>
-     
-    <h2 >Edit ST Profile</h2>
-    
-   <!-- <v-card-text>
-                <v-form>
-                  <v-text-field prepend-icon="person" name="fname" label="ชื่อ:" type="text"></v-text-field>
-                  <v-text-field prepend-icon="person" name="lname" label="นามสกุล" id="lname" type="text"></v-text-field>
-                  <v-text-field prepend-icon="person" name="department" label="สาขาวิชา" id="department" type="text"></v-text-field>
-                </v-form>
-              </v-card-text>
-    
-    <ul>
-      <li  v-for="st in student" :key="st.code">
-        <span>{{ st.code }}</span>
-        <span>{{ st.name }}</span>
-      </li>
-    </ul> -->
-
-    <h1>Student Edit</h1>
+      <h1>Student Edit</h1>
       <v-text-field v-model="fname" label="ชื่อ" />
      <v-text-field v-model="lname" label="นามสกุล" />
-     <v-text-field v-model="department" label="สาขาวิชา" />
     <v-btn @click="save">บันทึก</v-btn>
-
-    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -39,8 +19,7 @@ export default {
       this.fname = res.data.student.fname || ''
     this.lname = res.data.student.lname || ''
   },
-
-      methods: {
+  methods: {
       async save() {
           let res = await this.$http.post('/student/save',{
               id: this.$route.query.id,
@@ -54,7 +33,7 @@ export default {
               //ToDo: แสดงข้อความว่าบันทึกสำเร็จ
           }
       },
-    },
+  },
    layout: 'pageEdit'
 
   }
